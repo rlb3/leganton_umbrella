69328865dfaae130c3a2105ece6b6a811b3b2fe1
Robert Boone <robert@rlb3.com>
2017-06-23 10:55:33 -0500

    Add data retrieval functions

--- apps/feed_reader/lib/feed_reader/reader.ex
+++ apps/feed_reader/lib/feed_reader/reader.ex
@@ -7,27 +7,37 @@ defmodule FeedReader.Reader do
     GenServer.start_link(__MODULE__, nil, [])
   end
 
-  def get_site(pid, url) do
-    GenServer.cast(pid, {:get_site, url})
+  def fetch_site(pid, url) do
+    GenServer.cast(pid, {:fetch_site, url})
+  end
+
+  def get_site(pid) do
+    GenServer.call(pid, :site)
+  end
+
+  def get_feed(pid) do
+    GenServer.call(pid, :feed)
   end
 
   def init(_) do
     {:ok, %{site: nil, feed: nil}}
   end
 
-  def handle_cast({:get_site, url}, state) do
+  def handle_cast({:fetch_site, url}, state) do
     Process.send_after(self(), :get_feed, @wait_time)
     {:noreply, %{state | site: Scrape.website(url)}}
   end
 
-  def handle_call(:state, _from, state) do
-    {:reply, state, state}
+  def handle_call(:site, _from, state) do
+    {:reply, state.site, state}
   end
 
-  def handle_info(:get_feed, %{site: %{feeds: feeds}} = state) do
-    feed = Enum.flat_map(feeds, fn feed ->
-      Scrape.feed(feed)
-    end)
+  def handle_call(:feed, _from, state) do
+    {:reply, state.feed, state}
+  end
+
+  def handle_info(:get_feed, %{site: %{feeds: feed_urls}} = state) do
+    feed = Enum.flat_map(feed_urls, fn url -> Scrape.feed(url) end)
     {:noreply, %{state | feed: feed}}
   end
 end
